9c878fab7999a28d34576aeb6336215f48f7b11c
Flemming Hansen <gnimmelf@gmail.com>
2014-08-12 12:49:42 +0200

    release 0.8.0

--- test/01-es-setup.js
+++ test/01-es-setup.js
@@ -8,6 +8,7 @@ var request = require('supertest');
 var koa = require('koa');
 var co = require('co');
 var exec = require('child_process').exec;
+var fs = require('fs')
 
 var esGenClientX = require('../lib/es-gen-client-x.js');
 
@@ -17,6 +18,12 @@ console.log('NODE_ENV: '+process.env.NODE_ENV);
 if ( process.env.NODE_ENV !== 'test' ) {
     console.log("Woops, you want NODE_ENV=test before you try this again!");
     process.exit(1);
+}
+
+function readFile(file) {
+  return function(fn){
+    fs.readFile(file, 'utf8', fn);
+  }
 }
 
 describe('esGenClientX', function() {
@@ -143,6 +150,30 @@ describe('esGenClientX', function() {
           "hostname":"abareness.no",
           "image_base_url": "https://s3-eu-west-1.amazonaws.com/fireshop/abareness"
         }
+      })
+
+      // http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-indices-refresh
+      res = yield client.indices.refresh()
+
+      done()
+    })()
+
+  })
+
+  it("created `abareness/pages` ", function(done) {
+
+    var res
+
+    co(function*() {
+
+      console.log('Current directory: ' + process.cwd());
+
+      var data = yield readFile('./data/pages.json.txt');
+      data = JSON.parse(data)
+
+      var bulk_request = client.x.make_bulk_request(data, 'abareness', 'page')
+      var res = yield client.bulk({
+        body: bulk_request
       })
 
       // http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-indices-refresh
